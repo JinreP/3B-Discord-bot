35d2530ae72c3d01d9db3728a847c703c771c190
JinreP <subeegod@gmail.com>
2025-09-29 15:19:58 +0800

    id added

--- src/send-message.js
+++ src/send-message.js
@@ -19,36 +19,49 @@ const client = new Client({
 });
 const roles = [
   {
-    id: "1421850079492313248",
-    label: "test",
+    id: "1421849211699204116",
+    label: "Full-Stack Dev",
   },
 
   {
-    id: "1421850088258670672",
-    label: "sda",
+    id: "1421849210822590524",
+    label: "Backend Builder",
+  },
+  {
+    id: "1421849212525613236",
+    label: "Bug Slayer",
+  },
+
+  {
+    id: "1421849116249423913",
+    label: "Frontend Wizard",
   },
 ];
 client.on("clientReady", async (c) => {
   try {
-    const channel = await client.channels.cache.get("1399683254977822765");
+    const channel = await client.channels.cache.get("1399726798341931089");
     if (!channel) {
       console.log("Channel not found!");
       return;
     }
-    const row = new ActionRowBuilder();
-
-    roles.forEach((role) => {
-      row.components.push(
-        new ButtonBuilder()
-          .setCustomId(role.id)
-          .setLabel(role.label)
-          .setStyle(ButtonStyle.Success)
-      );
-    });
+    // Split buttons into rows of max 5
+    const rows = [];
+    for (let i = 0; i < roles.length; i += 5) {
+      const row = new ActionRowBuilder();
+      roles.slice(i, i + 5).forEach((role) => {
+        row.addComponents(
+          new ButtonBuilder()
+            .setCustomId(role.id)
+            .setLabel(role.label)
+            .setStyle(ButtonStyle.Success)
+        );
+      });
+      rows.push(row);
+    }
 
     await channel.send({
       content: "Claim or remove role",
-      components: [row],
+      components: rows,
     });
 
     process.exit();
